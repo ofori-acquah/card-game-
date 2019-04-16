87cdd9be46b84f44851a6d219c0e0c8a88416cc7
Papa Kofi <pkoforiacquah@gmail.com>
2019-04-16 21:22:59 +0000

    First commit for card-g

---
+++ MainCard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCard : MonoBehaviour {
+
+
+    [SerializeField] private GameObject Card_Back;
+
+    public void OnMouseDown()
+    {
+        if(Card_Back.activeSelf)
+        {
+            Card_Back.SetActive(false);
+            
+        }
+    }
+}
